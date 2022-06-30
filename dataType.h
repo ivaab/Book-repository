c24dbded95ab16ece09840096b5c1ecd4e1f30b2
Iva <105794652+ivaab@users.noreply.github.com>
2022-07-01 01:38:19 +0200

    Update dataType.h

--- dataType.h
+++ dataType.h
@@ -1,11 +1,40 @@
-#ifndef DATATYPE_H
-#define DATATYPE_H
 
-typedef struct book{
-    char name[50];
-    char autor[30];
-    char genre[30];
-    char year[30];
-}BOOK;
+#include <stdio.h>
+#include <stdlib.h>
+#include "dataType.h"
 
-#endif DATATYPE_H
+
+int main() {
+
+
+    int n;
+
+    printf("***********************************\n");
+    printf("*                                 *\n");
+    printf("*        BOOK REPOSITORY          *\n");
+    printf("*                                 *\n");
+    printf("***********************************\n");
+
+    do{
+        printf("\n1) Add book \n");
+        printf("2) Print all books \n");
+        printf("3) Delete book \n");
+        printf("4) Print author \n");
+        printf("5) Print genre \n");
+        printf("6) Book search \n");
+        printf("7) Delete repository \n");
+        printf("8) Search \n");
+        printf("9) Save changes & Exit \n");
+
+        do {
+            printf("\nEnter number: ");
+            scanf("%d", &n);
+            getchar();
+        }while (n<1 || n>9);
+
+        switchCase(n);
+
+    }while(n != 9);
+
+    return 0;
+}
