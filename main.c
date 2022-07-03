fdb98aac1b4dd31ede1de006f95ac4bbb4ccfd38
Iva <105794652+ivaab@users.noreply.github.com>
2022-07-03 18:03:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "functions.h"
 #include "dataType.h"
 
+
 int main() {
+
 
     int n;
 
@@ -17,22 +18,22 @@ int main() {
         printf("\n1) Add book \n");
         printf("2) Print all books \n");
         printf("3) Delete book \n");
-        printf("4) Print autor \n");
+        printf("4) Print author \n");
         printf("5) Print genre \n");
-        printf("6) Print year \n");
-        printf("7) Book search \n");
-        printf("8) Delete repository \n");
-        printf("9) Search \n");
-        printf("10) Save changes & Exit \n");
+        printf("6) Book search \n");
+        printf("7) Delete repository \n");
+        printf("8) Search \n");
+        printf("9) Save changes & Exit \n");
 
         do {
             printf("\nEnter number: ");
             scanf("%d", &n);
             getchar();
-        }while (n<1 || n>10);
+        }while (n<1 || n>9);
 
+        switchCase(n);
 
-    }while(n != 10);
+    }while(n != 9);
 
     return 0;
 }
